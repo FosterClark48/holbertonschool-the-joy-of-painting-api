8ddc3cc531e2c89be4d3c2ab81e1805f199bcb88
FosterClark48 <fosterclark48@yahoo.com>
2023-12-06 22:15:41 -0600

    Script that creates the EpisodeColor class for the API to query from

---
+++ models/episode_color.py
@@ -0,0 +1,14 @@
+from .base import Base
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class EpisodeColor(Base):
+  __tablename__ = 'EpisodeColors'
+
+  EpisodeID = Column(String(10), ForeignKey('Episodes.EpisodeID'), primary_key=True)
+  ColorID = Column(Integer, ForeignKey('Colors.ColorID'), primary_key=True)
+
+  # Relationships
+  episode = relationship("Episode", back_populates="colors")
+  color = relationship("Color", back_populates="episodes")
